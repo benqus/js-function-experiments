f1a62721512a6f2253c952d5ab333728d808f322
benqus <ben.kormos@gmail.com>
2020-07-31 18:05:16 +0100

    initial examples

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 ////////////////
 // Example #1 //
 ////////////////
+console.log('');
 console.log('Example', 1);
 
 function a() {
-  console.log(this);
+  console.log('a', this);
 }
 a();
 
@@ -14,16 +15,20 @@ const o1 = {
 // or shorter: const o1 = { a };
 o1.a();
 
+// alternatively
+a.call(o1);
+
 
 
 ////////////////
 // Example #2 //
 ////////////////
+console.log('');
 console.log('Example', 2);
 
 const o2 = {
   b() {
-    console.log(this);
+    console.log('b', this);
   }
 };
 o2.b();
@@ -40,49 +45,69 @@ b();
 ////////////////
 // Example #3 //
 ////////////////
+console.log('');
 console.log('Example', 3);
 
 const o3 = {
-  d() {
-    const _d = () => console.log(this);
-    _d();
+  c() {
+    const _c = () => console.log('c', this);
+    _c();
   }
 }
 
-const { d } = o3;
-o3.d();
-d();
+const { c } = o3;
+o3.c();
+c();
 
 
 
 ////////////////
 // Example #4 //
 ////////////////
+console.log('');
 console.log('Example', 4);
 
 const o4 = {
-  c: () => {
-    console.log(this);
+  d: () => {
+    console.log('d', this);
   }
 };
-o4.c();
+o4.d();
 
-const { c } = o4;
-c();
+const { d } = o4;
+d();
 
 
 
 ////////////////
 // Example #5 //
 ////////////////
+console.log('');
 console.log('Example', 5);
 
 class Ipsum {
-  e = () => {
-    console.log(this);
-  }
+  
+  e() {
+    console.log('e', this);
+  };
+
+  f = () => {
+    console.log('f', this);
+  };
+
+  g = (context) => {
+    console.log('g', this === context);
+  };
+
 }
+
 const o5 = new Ipsum();
-const { e } = o5;
+const { e, f, g } = o5;
 o5.e();
 e();
+
+o5.f();
+f();
+
+o5.g(o5);
+g(o5);
